c2369c755cedc3facd03cf1dde6c79d932e80ea2
Jacqueline Kurien <jacquelinekurien@AdminisatorsMBP.attlocal.net>
2020-03-30 11:20:30 -0400

    Array Exercise

---
+++ Checkpoint_Arrays/script.js
@@ -0,0 +1,11 @@
+// 3 array methods are 
+// .shift()
+// .pop()
+// .push()
+
+var drinks=[];
+var drinks=["Water", "Coffee", "Tea"];
+var foods=[];
+var foods=["Apple", "Sandwich", "Chips"];
+var meals= drinks.concat(foods);
+console.log(meals.reverse());
